0f174028d4c9ab2662c229510de741fa87c00474
Kenneth Rakentine <specialistmorgenj@gmail.com>
2023-11-12 04:09:46 -0500

    importing logo png via assets dir

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import mainLogo from '../assets/mainLogo.png'
+
 
 function Top() {
   return (
     <div className='topContain'>
-        <img className="mainLogo" src="https://i.ibb.co/HqZSYxp/PODPHONIC-MIC-LOGO-COMPLETE-1-11-11-23.png" alt="main logo" />
+        {/* <img className="mainLogo" src="https://i.ibb.co/HqZSYxp/PODPHONIC-MIC-LOGO-COMPLETE-1-11-11-23.png" alt="main logo" /> */}
+
+        <img className="mainLogo" src={mainLogo} alt="main logo" />
+
+        
     </div>
   )
 }
